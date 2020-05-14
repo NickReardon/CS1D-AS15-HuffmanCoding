c8e877e7ff63ae8a9690d75757ce61a0267a23eb
Nick Reardon <32754140+ThingOfNightmare@users.noreply.github.com>
2020-05-13 19:56:32 -0700

    Add project files.

---
+++ CS1D-AS15-HuffmanCoding/main.cpp
@@ -0,0 +1,28 @@
+/*****************************************************************************
+ * AUTHOR 	  		: Nick Reardon
+ * Assignment #6	: Priority Queues
+ * CLASS			: CS1D
+ * SECTION			: MW - 2:30p
+ * DUE DATE			: 02 / 24 / 20
+ *****************************************************************************/
+#include "main.h"
+#include <queue>
+using std::cout; using std::endl;
+
+
+int main()
+{
+
+	/*
+	 * HEADER OUTPUT
+	 */
+	PrintHeader(cout, "Prompt.txt");
+
+	/********************************************************************/
+
+	
+
+	system("pause");
+	return 0;
+}
+
